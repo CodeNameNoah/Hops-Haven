480189afa10f776ac6294cf5d27ebfa9a15aab79
Noah Hoang <hoincbusiness@gmail.com>
2023-05-09 13:31:28 -0700

    Created connection.js

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    });
+
+module.exports = sequelize;
