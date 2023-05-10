a9627684b9f917fbe29bf87f0d1f7c7eb2e68234
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-05-09 18:03:35 -0700

    login and signup handlebars layout and accompanying javascript

---
+++ public/js/signup.js
@@ -0,0 +1,24 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
+  document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
