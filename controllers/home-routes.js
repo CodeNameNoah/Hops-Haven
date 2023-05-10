4e8366c45776f33e3871478d6c32bcc2e52ca5e5
Noah Hoang <hoincbusiness@gmail.com>
2023-05-10 16:00:51 -0700

    Revised code in multiple files, made minor aesthetic changes to main page handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,7 +41,7 @@ router.get('/', async (req, res) => {
       // Pass serialized data and session flag into template
       res.render('homepage', {
         randomBeers,
-        logged_in: true
+        logged_in: req.session.logged_in
       });
     } catch (err) {
       res.status(500).json(err);
@@ -51,7 +51,7 @@ router.get('/', async (req, res) => {
   router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/homepage');
+      res.redirect('/');
       return;
     }
     res.render('login');
@@ -61,7 +61,7 @@ router.get('/', async (req, res) => {
 router.get('/signup', (req, res) => {
   // If the user is already logged in, redirect the request to another route
    if (req.session.logged_in) {
-     res.redirect('/homepage');
+     res.redirect('/');
      return;
    }
    res.render('signup');
--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
