f201aef2e96712aa8f85703480e74ff5485df0c6
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-05-10 14:43:27 -0700

    deleted unecessary comments

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -27,34 +27,8 @@ router.get("/", withAuth, async (req, res) => {
   }
 });
 
-// // Render the 'new-post' template for creating a new post at '/new' endpoint
-// router.get("/new", withAuth, (req, res) => {
-//   res.render("new-post", {
-//     layout: "main",
-//   });
-// });
 
-// // Render the 'edit-post' template for editing a specific post at '/edit/:id' endpoint
-// router.get("/edit/:id", withAuth, async (req, res) => {
-//   try {
-//     // Find the post by Id
-//     const postData = await Post.findByPk(req.params.id);
 
-//     if (postData) {
-//       // If post exists, render 'edit-post' template and pass in post data
-//       const post = postData.get({ plain: true });
-//       res.render("edit-post", {
-//         layout: "main",
-//         post,
-//       });
-//     } else {
-//       // If no post, send 404 status
-//       res.status(404).end();
-//     }
-//   } catch (err) {
-//     res.redirect("login");
-//   }
-// });
 
 // Export router
 module.exports = router;
