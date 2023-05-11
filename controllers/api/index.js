374528300c39defd01aaf7edc234f1d011b55b4f
The Duz <tamara.dusenbury@gmail.com>
2023-05-11 16:42:20 -0700

    added beerToTry and favBeer routes; added BeerToTry model; began BeerToTry and FavBeer js files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,11 +4,13 @@ const router = require('express').Router();
 // Importing User, Post & Comment routes
 const userRoutes = require('./user-routes.js');
 const randomBeerRoutes = require('./randomBeer-routes.js');
-// const favBeerRoutes = require('./favBeer-routes.js');
+const favBeerRoutes = require('./favBeer-routes.js');
+const beerToTryRoutes = require('./beerToTry-routes.js')
 
 // Endpoints
 router.use('/users', userRoutes);
 router.use('/randomBeer', randomBeerRoutes);
-// router.use('/favBeers', favBeerRoutes);
+router.use('/favBeers', favBeerRoutes);
+router.use('/beerToTry', beerToTryRoutes);
 
 module.exports = router;
